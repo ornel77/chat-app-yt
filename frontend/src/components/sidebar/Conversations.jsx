55f55bd86f01f52a72092f76d7cb85d83ceaa1a9
ornel77 <t.ornel@yahoo.fr>
2024-02-12 00:11:09 +0100

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,15 @@
+import Conversion from "./Conversion"
+
+const Conversations = () => {
+  return (
+    <div className="flex py-2 flex-col overflow-auto">
+        <Conversion />
+        <Conversion />
+        <Conversion />
+        <Conversion />
+        <Conversion />
+        <Conversion />
+    </div>
+  )
+}
+export default Conversations
